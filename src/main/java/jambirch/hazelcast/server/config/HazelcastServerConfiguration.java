9862266d86c882f536602aac888345b93b20628e
Peter Jamieson <peter.jamieson@sainsburys.co.uk>
2017-01-30 16:25:45 +0000

    Allowing UDP or TCP based on profile

--- src/main/java/jambirch/hazelcast/server/config/HazelcastServerConfiguration.java
+++ src/main/java/jambirch/hazelcast/server/config/HazelcastServerConfiguration.java
@@ -2,21 +2,29 @@ package jambirch.hazelcast.server.config;
 
 import com.hazelcast.config.Config;
 import com.hazelcast.config.JoinConfig;
-import com.hazelcast.config.MapConfig;
 import com.hazelcast.config.NetworkConfig;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource("classpath:application.properties")
 public class HazelcastServerConfiguration {
+    @Bean
+    @Profile("udp")
+    public Config udpConfig() {
+        return new Config();
+    }
 
     @Bean
-    public Config config() {
+    @Profile("tcp")
+    public Config tcpConfig(@Value("${tcp.member:127.0.0.1}") String tcpMember) {
         Config hazelcastConfig = new Config();
-        NetworkConfig netConf = hazelcastConfig.getNetworkConfig();
-        JoinConfig joinCfg = netConf.getJoin();
+        JoinConfig joinCfg = hazelcastConfig.getNetworkConfig().getJoin();
         joinCfg.getMulticastConfig().setEnabled(false);
-        joinCfg.getTcpIpConfig().setEnabled(true).addMember("127.0.0.1");
+        joinCfg.getTcpIpConfig().setEnabled(true).addMember(tcpMember);
 
         return hazelcastConfig;
     }
